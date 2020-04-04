ad7877fc7440890eadf0d0537590fcb3d5b00717
metaTinker <michael.roust@gmail.com>
2020-04-05 01:38:29 +0200

    m

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 # The pytorio_playground "main"
 
-from pytorio.calculator import *
 import pprint
+
+import easy_io
+from pytorio import encoding, calculator
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -13,36 +15,23 @@ def save_dict(ddict):
     easy_io.write_file(json_str, 'src/pytorio/resources/misc/' + 'dict.json')
 
 
-# pp.pprint(recipes)
-# pp.pprint(recipes[find_item_recipe('iron-plate')])
+def decode_blueprint_file(filename_in, filename_out):
+    exg_str = easy_io.read_file(filename_in)
+    json_str = encoding.exg_str_to_json_str(exg_str)
+    json_str = encoding.beatify_json(json_str)
+    easy_io.write_file(json_str, filename_out)
 
-shared_items = ['iron-plate', 'copper-plate', 'crude-oil', 'water', 'sulfuric-acid', 'light-oil', 'heavy-oil']
-prefered_recipes = {'petroleum-gas': 'advanced-oil-processing', 'solid-fuel': 'solid-fuel-from-light-oil'}
 
-# recipe_tree = build_recipe_tree(5, 'iron-gear-wheel')
-# recipe_tree = build_recipe_tree(5, 'iron-gear-wheel', 'item', shared_items=['iron-plate'])
-# recipe_tree = build_recipe_tree(1, 'advanced-circuit', 'item', shared_items, prefered_recipes)
-# recipe_tree = build_recipe_tree(0.1, 'rocket-part', 'item', shared_items, prefered_recipes)
+def encode_blueprint_file(filename_in, filename_out):
+    pass
 
-# pp.pprint(recipe_tree)
-# save_dict(recipe_tree)
 
-# pp.pprint()
-# pp.pprint(find_all_item_recipes('petroleum-gas'))
+def foo():
+    folder = 'src/pytorio/resources/example_blueprints/'
+    filename_in = folder + 'fluids1.txt'
+    filename_out = folder + 'fluids1.json'
 
-# ====================================================================================
+    decode_blueprint_file(filename_in, filename_out)
 
-prefered_machines = ['assembling-machine-3']
-# module_priorities = {'assembling-machine-3': {'module_priority': [{'productivity-module-3': 4}, {'speed-module-3': 4}]}}
 
-# def multiplier_calculator
-
-# prod_tree = build_production_tree(1, 'item', 'advanced-circuit', shared_items, prefered_recipes, prefered_machines)
-prod_tree = build_production_tree(1, 'item', 'processing-unit', shared_items, prefered_recipes, prefered_machines)
-# prod_tree = build_production_tree(0.1, 'item', 'rocket-part', shared_items, prefered_recipes)
-
-pp.pprint(prod_tree)
-save_dict(prod_tree)
-
-# pp.pprint()
-# pp.pprint(find_all_item_recipes('petroleum-gas'))
+foo()

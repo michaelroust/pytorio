5716e40e9295b0865cd0cbf620cccf3d6f9094af
metaTinker <michael.roust@gmail.com>
2020-04-06 19:12:59 +0200

    added items to Entity

--- src/pytorio/blueprint.py
+++ src/pytorio/blueprint.py
@@ -2,6 +2,7 @@ from enum import IntEnum
 
 # ============================================================================
 
+
 class Position:
     def __init__(self, x, y):
         self.x = x
@@ -15,13 +16,15 @@ class Position:
 
     @staticmethod
     def to_dict(position):
-        return {"x":position.x, "y":position.y}
+        return {"x": position.x, "y": position.y}
 
     @staticmethod
     def from_dict(pos_dict):
         return Position(pos_dict["x"], pos_dict["y"])
 
+
 # ============================================================================
+
 
 class Direction(IntEnum):
     NORTH = 0
@@ -29,7 +32,9 @@ class Direction(IntEnum):
     SOUTH = 4
     WEST = 6
 
+
 # ============================================================================
+
 
 class Logistic_Filter:
     def __init__(self, name, count):
@@ -42,7 +47,6 @@ class Logistic_Filter:
     def __repr__(self):
         return self.__class__.__name__ + self.__str__()
 
-
     @staticmethod
     def generate_indexes(logistic_filter_list):
         """Takes a list of logistic filters and correctly given them each and index."""
@@ -51,27 +55,34 @@ class Logistic_Filter:
             logistic_filter.index = i
             i += 1
 
-
     @staticmethod
     def to_dict(logistic_filter):
         return {"name": logistic_filter.name, "index": logistic_filter.index, "count": logistic_filter.count}
-
 
     @staticmethod
     def from_dict(lf_dict):
         return Logistic_Filter(lf_dict["name"], lf_dict["count"])
 
+
 # ============================================================================
 
+
 class Entity:
-    def __init__(self, name:str, position:Position, direction:Direction=None, bar:int=None, recipe:str=None, request_filter_list:list=None):
+    def __init__(self,
+                 name: str,
+                 position: Position,
+                 direction: Direction = None,
+                 bar: int = None,
+                 recipe: str = None,
+                 request_filter_list: list = None,
+                 items: dict = None):
         self.name = name
         self.position = position
         if direction != None: self.direction = direction
         if bar != None: self.bar = bar
         if recipe != None: self.recipe = recipe
         if request_filter_list != None: self.request_filters = request_filter_list
-
+        if items != None: self.items = items
 
     def __str__(self):
         e_str = "(name: {},  {}".format(self.name, repr(self.position))
@@ -80,17 +91,16 @@ class Entity:
         if hasattr(self, "bar"): e_str += ",  bar: " + str(self.bar)
         if hasattr(self, "recipe"): e_str += ",  recipe: " + self.recipe
         if hasattr(self, "request_filters"): e_str += ",  " + str(self.request_filters)
+        if hasattr(self, "items"): e_str += ", " + str(self.items)
         e_str += ")"
 
         return e_str
 
-
     def __repr__(self):
         return self.__class__.__name__ + self.__str__()
 
-
     @staticmethod
-    def generate_all_numbers(entity_list:list):
+    def generate_all_numbers(entity_list: list):
         """Generates all the entity_numbers/indexes for contents of this entity"""
         i = 1
         for entity in entity_list:
@@ -98,7 +108,6 @@ class Entity:
             i += 1
 
             if hasattr(entity, "request_filters"): Logistic_Filter.generate_indexes(entity.request_filters)
-
 
     @staticmethod
     def to_dict(entity):
@@ -112,11 +121,13 @@ class Entity:
         if hasattr(entity, "recipe"): entity_dict.update({"recipe": entity.recipe})
         if hasattr(entity, "request_filters"):
             entity_dict.update({
-                "request_filters": [Logistic_Filter.to_dict(logistic_filter) for logistic_filter in entity.request_filters]
+                "request_filters":
+                [Logistic_Filter.to_dict(logistic_filter) for logistic_filter in entity.request_filters]
             })
+        if hasattr(entity, "items"):
+            entity_dict.update({"items": entity.items})
 
         return entity_dict
-
 
     @staticmethod
     def from_dict(entity_dict):
@@ -125,24 +136,25 @@ class Entity:
         recipe = entity_dict.get("recipe")
         request_filters = [Logistic_Filter.from_dict(lf_dict) for lf_dict in entity_dict.get("request_filters")]
 
-        return Entity(entity_dict["name"], Position.from_dict(entity_dict["position"]), direction, bar, recipe, request_filters)
+        return Entity(entity_dict["name"], Position.from_dict(entity_dict["position"]), direction, bar, recipe,
+                      request_filters)
+
 
 # ============================================================================
+
 
 class Blueprint:
     default = {
         "blueprint": {
             "entities": [],
             "tiles": [],
-            "icons": [
-                {
-                    "index": 1,
-                    "signal": {
-                        "name": "signal-info",
-                        "type": "virtual"
-                    }
+            "icons": [{
+                "index": 1,
+                "signal": {
+                    "name": "signal-info",
+                    "type": "virtual"
                 }
-            ],
+            }],
             "item": "blueprint",
             "label": "main",
             "version": 73017917444
@@ -150,7 +162,7 @@ class Blueprint:
     }
 
     # TODO make Blueprint a proper class, and not a hack!!!
-    def __init__(self, entities:list):
+    def __init__(self, entities: list):
         self.entities = entities
 
     # def add_entity(self, entity):
